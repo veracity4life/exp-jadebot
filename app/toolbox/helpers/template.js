b0f26c5d99e64da8c5be944e1ad247e2d3deef11
Michael Russell <veracity4life@gmail.com>
2016-04-28 12:42:47 -0500

    Rewrite templateMatch to be a factory function

--- app/toolbox/helpers/template.js
+++ app/toolbox/helpers/template.js
@@ -1,59 +1,96 @@
-function getTemplateMatch(templates, string) {
-    let target = 0.66,
+function templateMatch(params) {
+    'use strict';
+
+    params = params || {};
+
+    let percentTarget,
         percentWords = 0.00,
-        percentPos = 0.00;
+        percentPos = 0.00,
+        input = '',
+        templates = [];
 
-    let returnVal = {
-            string: string,
-            template: '',
-            hasMatch: false,
-            confidence: 0.00
-        };
+    input = params.string;
+    templates = params.templates;
+    percentTarget = params.confidence || 0.67;
 
-    let sWords = _.split(_.toLower(string), ' ')
-
-    if(!_.isArray(templates) || !_.isString(string)) {
-        return console.debug("Templates or String args not of correct type.");
+    if(!_.isArray(templates) || !_.isString(input)) {
+        return handleError('Templates must be an array and Input must be a string');
     }
 
-    _.map(templates, function (row) {
-        let w = _.split(row, ' '),
-            subW = 0.00,
-            subP = 0.00;
+    function handleError(e) {
+        return console.error("Error: " + e);
+    }
 
-        _.forEach(w, function (value, key) {
-            let i = _.indexOf(sWords, _.toLower(value), key);
+    return findTempalte = {
+        results: {
+            input: '',
+            template: '',
+            isMatch: false,
+            confidence: 0.00
+        },
+        setInputString: function (str) {
+            if(!_.isString(str)) {
+                return handleError('Input must be a string');
+            }
 
-            if(i !== -1 || /%(\w*)%/ig.test(value)) {
-                subW += (1/w.length);
+            input = str || input;
+        },
+        getInputString: function () {
+            return input;
+        },
+        setTemplates: function (obj) {
+            if(!_.isArray(templates)) {
+                return handleError('Templates must be an array');
+            }
 
-                if(i !== key) {
-                    if(w[key+1] != undefined && (w[key+1] === sWords[i+1] || /%(\w*)%/ig.test(w[key+1]))) {
-                        subP += (1/w.length);
+            templates = obj || templates;
+        },
+        getTemplates: function () {
+            return templates;
+        },
+        getMatchingTemplate: function () {
+            let sWords = _.split(_.toLower(input), ' ');
+
+            _.map(templates, function (row) {
+                let w = _.split(row, ' '),
+                    subW = 0.00,
+                    subP = 0.00;
+
+                _.forEach(w, function (value, key) {
+                    let i = _.indexOf(sWords, _.toLower(value), key);
+
+                    if(i !== -1 || /%(\w*)%/ig.test(value)) {
+                        subW += (1/w.length);
+
+                        if(i !== key) {
+                            if(w[key+1] !== undefined && (w[key+1] === sWords[i+1] || /%(\w*)%/ig.test(w[key+1]))) {
+                                subP += (1/w.length);
+                            }
+                        } else {
+                            subP += (1/w.length);
+                        }
                     }
-                } else {
-                    subP += (1/w.length);
+                    // console.debug({row:row, i:i, value:value, subW:subW, subP:subP});
+                });
+
+                if(subW > percentWords) {
+                    percentWords = subW;
+                    percentPos = subP;
+                    this.results.template = row;
+                    
+                    if(w.length != sWords.length) {
+                        percentWords = percentWords * (0.85);
+                    }
                 }
-            }
-            // console.debug({row:row, i:i, value:value, subW:subW, subP:subP});
-        });
+            });
 
-        if(subW > percentWords) {
-            percentWords = subW;
-            percentPos = subP;
-            returnVal.template = row;
-            
-            if(w.length != sWords.length) {
-                percentWords = percentWords * (.85);
-            }
+            this.results.confidence = _.round((percentWords * 0.6) + (percentPos * 0.4), 4);
+
+            if(this.results.confidence >= target)
+                this.results.isMatch = true;
+        },
+        getResults: function () {
+            return this.results;
         }
-    });
-
-    returnVal.confidence = _.round((percentWords * .6) + (percentPos * .4), 4);
-    // console.debug(returnVal);
-
-    if(returnVal.confidence >= target)
-        returnVal.hasMatch = true;
-
-    return returnVal;
+    };
 }
