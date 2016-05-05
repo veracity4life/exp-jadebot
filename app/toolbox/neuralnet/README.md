19628a07dcd10045d13d7512c2a6b5d51c269fa5
Michael Russell <veracity4life@gmail.com>
2016-05-05 16:04:49 -0500

    Rewrite neural network code

---
+++ app/toolbox/neuralnet/README.md
@@ -0,0 +1,13 @@
+## Implementation
+
+Using ```neuralnet.js``` to test a simple neural network implementation.
+```
+var n = neuralnet({"trainingInputs": [[0,0,1],[1,1,1],[1,0,1],[0,1,1]], "trainingOutputs": [0,1,1,0]});
+n.runTrainingProcess(10000);
+n.testNewInput([1,0,0]);
+```
+Output for the test after 10,000 iterations is expected to be 1.  Actual returned value is approx ```0.99993``` so its very close after that many iterations.
+
+I cannot take full credit as I basically converted this post into my own JS version:
+
+https://medium.com/technology-invention-and-more/how-to-build-a-simple-neural-network-in-9-lines-of-python-code-cc8f23647ca1#.6xelgghyn
---
+++ app/toolbox/neuralnet/neuralnet.js
@@ -0,0 +1,119 @@
+function neuralnet(params) {
+    'use strict';
+
+    params = params || {};
+
+    const neuron_VERSION = '0.2.0';
+
+    let weights = [],
+        trainingInputs = params.trainingInputs,
+        trainingOutputs = params.trainingOutputs;
+
+    let traingingOutputVals,
+        input = [],
+        outputVal = [];
+
+    function sigmoidFn(n) {
+        return 1 / ( 1 + Math.exp(-n) );
+    }
+
+    function sigmoidDerivativeFn(n) {
+        return n * (1 - n);
+    }
+
+    function initWeights() {
+        if (weights === [] || weights.length != trainingInputs[0].length) {
+            _.forEach(trainingInputs[0], function (v,k) {
+                addNewWeight(k);
+            });
+        }
+    }
+
+    function addNewWeight(key) {
+        weights.push(Math.random());
+    }
+
+    function adjustWeights(args) {
+        let index = args[0],
+            error = args[1],
+            temps = [],
+            adjustment;
+
+        adjustment = error * sigmoidDerivativeFn(outputVal[index]);
+
+        _.forEach(trainingInputs[index], function (value, key) {
+            weights[key] += value * adjustment;
+        }); 
+    }
+
+    function trainingProcess() {
+        let output = [],
+            temps = [];
+
+        let tOutput = trainingOutputs,
+            tInput = trainingInputs;
+
+        initWeights();
+
+        _.forEach(tInput, function (value, key) {
+            _.forEach(value, function (v, k) {
+                temps[key] = !_.isArray(temps[key]) ? [] : temps[key];
+                temps[key].push(v * weights[k]);
+            });
+        });
+
+        _.forEach(temps, function (value) {
+            output.push(sigmoidFn(_.sum(value)));
+        });
+
+        outputVal = output;
+
+        _.forEach(output, function (v, k) {
+            let error = tOutput[k] - v;
+
+            if (error != 0) {
+                adjustWeights([k, error]);
+            }
+        });
+
+        return outputVal;
+    }
+
+    function getOutputFrom(input) {
+        let set = input || [],
+            temps = [],
+            output = undefined;
+
+        if (set === [])
+            return console.log('Set is underfined.');
+
+        _.forEach(set, function (v, k) {
+            temps.push(v * weights[k]);
+        });
+
+        output = sigmoidFn(_.sum(temps));
+
+        return output;
+    }
+
+    return {
+        runTrainingProcess : function (iterations) {
+            do {
+                trainingProcess();
+                iterations -= 1;
+            } while (iterations > 0)
+        },
+        getOutput : function () {
+            return outputVal;
+        },
+        getWeights : function () {
+            return weights;
+        },
+        setWeight : function (args) {
+            weights[args[0]] = args[1];
+        },
+        testNewInput: function (input) {
+            return getOutputFrom(input);
+        }
+    };
+}
